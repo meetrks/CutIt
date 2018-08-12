06b4f8fd4d5cd30c2cd1c9f75a24b4be4c0f0d28
Rahul <skrsinghrahul@gmail.com>
2018-08-12 18:41:55 +0530

    update robots.txt and sitemap.xml

--- cutit/urls.py
+++ cutit/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import url, include
+from django.views.generic.base import RedirectView
+
+robots_view = RedirectView.as_view(url='/st/robots.txt', permanent=True)
+sitemap_view = RedirectView.as_view(url='/st/sitemap.xml', permanent=True)
 
 urlpatterns = [
     url('', include('url_cut.urls')),
+    url(r'^robots.txt$', robots_view),
+    url(r'^sitemap.xml$', sitemap_view),
 ]

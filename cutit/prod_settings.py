5531b8fb9f2eb7470783722961688b30721c007c
Rahul <skrsinghrahul@gmail.com>
2018-08-16 01:33:34 +0530

    update prod settings- allow www redirecting

--- cutit/prod_settings.py
+++ cutit/prod_settings.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['cutit.tk']
+ALLOWED_HOSTS = ['cutit.tk', 'www.cutit.tk']
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '$c6wtefwk+awg8wdb$0zq6$6cc8u740xr#%)*@1hnj4pm3@e0z')
 

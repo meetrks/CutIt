8c1107d75f3bd18e8a0e2fa1ea5fe552856e9827
Rahul <skrsinghrahul@gmail.com>
2018-08-16 01:32:42 +0530

    update middleware- add gzip compression to for contents

--- cutit/base.py
+++ cutit/base.py
@@ -43,6 +43,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
 ]
 
 ROOT_URLCONF = 'cutit.urls'

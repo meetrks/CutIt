e6343231658dfa6941edb786f30c0118d3793077
Rahul <skrsinghrahul@gmail.com>
2018-08-16 00:45:40 +0530

    update favicon for site

--- cutit/urls.py
+++ cutit/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url, include
 from django.views.generic.base import RedirectView
 
+favicon_view = RedirectView.as_view(url='/st/logo/logo_36x36.png', permanent=True)
 robots_view = RedirectView.as_view(url='/st/robots.txt', permanent=True)
 sitemap_view = RedirectView.as_view(url='/st/sitemap.xml', permanent=True)
 
 urlpatterns = [
     url('', include('url_cut.urls')),
+    url(r'^favicon\.ico$', favicon_view),
     url(r'^robots.txt$', robots_view),
     url(r'^sitemap.xml$', sitemap_view),
 ]
